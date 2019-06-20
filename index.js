e368526668c6b591d1d09da5783d7296fed72fbe
Jason Whalley <jason.whalley@rapha.cc>
2019-06-20 17:17:19 +0100

    Update to run in Google Cloud function

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-checkSetMilestone = require("./lib/check-set-milestone");
-
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
-module.exports = app => {
-  app.on(
-    [
-      "pull_request.opened",
-      "pull_request.edited",
-      "pull_request.synchronize",
-      "issues.milestoned",
-      "issues.demilestoned"
-    ],
-    context => checkSetMilestone(context)
-  );
-};
+const { serverless } = require("@probot/serverless-gcf");
+const appFn = require("./probot.js");
+module.exports.probot = serverless(appFn);
